18054ff827934e32c254b6731a57e47580ef1f19
Artu211 <160549068+Artu211@users.noreply.github.com>
2025-10-28 20:56:31 +0100

    gotowe funkcje ataków

--- src/Astrologer.java
+++ src/Astrologer.java
@@ -1,4 +1,8 @@
-public class Astrologer extends Character {
+import javax.accessibility.AccessibleTable;
+import java.io.Serializable;
+import java.util.Random;
+
+public class Astrologer extends Character implements Attackable {
     int energy;
 
     public Astrologer(String name, int level, int health, int strength, int dexterity, int intelligence, int energy) {
@@ -11,14 +15,28 @@ public class Astrologer extends Character {
         this.energy = energy;
     }
 
-
-    public void normalAttack(Character character) {
-
+    @Override
+    public void normalAttack(Enemy enemy) {
+        Random r = new Random();
+        int dmg = r.nextInt(101) + intelligence;
+        enemy.health -= dmg;
+        System.out.println(name + " używa normalnego ataku i zadaje " + dmg + " obrażeń! (HP wroga: " + enemy.health + ")");
     }
 
+    @Override
+    public void superAttack(Enemy enemy) {
+        Random r = new Random();
+        int dmg = r.nextInt(501) + intelligence;
+        enemy.health -= dmg;
+        System.out.println(name + " używa super ataku i zadaje " + dmg + " obrażeń! (HP wroga: " + enemy.health + ")");
+    }
 
-    public void superAttack(Character character) {
-
+    @Override
+    public void Ult(Enemy enemy){
+        Random r = new Random();
+        int dmg = r.nextInt(1001) + intelligence;
+        enemy.health -= dmg;
+        System.out.println(name + " używa ulta i zadaje " + dmg + " obrażeń (HP wroga: " + enemy.health + ")");
     }
 
 }
